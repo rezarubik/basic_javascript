189b005e346af18692c9fd0cd8b42a4bb974ede7
Muhammad Reza Pahlevi Y <muhammad.reza.pahlevi.y@gmail.com>
2020-02-23 23:51:14 +0700

    terakhir sampai nested loop atau video ke 25

--- angkot1.js
+++ angkot1.js
@@ -1,5 +1,9 @@
-var nilaiAwal = 1;
-while (nilaiAwal <= 10) {
+var noAngkot = 1;
+var jumlahAngkot = 10;
+while (noAngkot <= jumlahAngkot) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+    noAngkot++;
+}
+for (var nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++) {
     console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik.');
-    nilaiAwal++;
 }
