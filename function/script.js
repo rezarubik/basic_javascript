e8788acbabea2ae2664e50e1a73bb9b06cb87daf
Muhammad Reza Pahlevi Y <muhammad.reza.pahlevi.y@gmail.com>
2020-05-12 00:08:16 +0700

    Last 8.0 Array Intro

--- function/script.js
+++ function/script.js
@@ -30,12 +30,78 @@
 //     ' Hasil = ' + hasilKali);
 
 // todo pseudo variable (variable sementara) pada javascript
-function tambahPseudo() {
-    var hasil = 0;
-    for (var i = 0; i < arguments.length; i++) {
-        hasil += arguments[i];
-    }
-    return hasil;
+// function tambahPseudo() {
+//     var hasil = 0;
+//     for (var i = 0; i < arguments.length; i++) {
+//         hasil += arguments[i];
+//     }
+//     return hasil;
+// }
+// var coba = tambahPseudo(1, 2, 3, 4, 5);
+// console.log(coba);
+
+// todo refactoring sederhana -> membuat program menjadi efisien
+// function jumlahDuaVolumeKubus(a, b) {
+//     return a * a * a + b * b * b;
+// }
+// var nilai1 = parseInt(prompt('Masukkan nilai 1 = '));
+// var nilai2 = parseInt(prompt('Masukkan nilai 2 = '));
+// console.log(jumlahDuaVolumeKubus(nilai1, nilai2));
+
+// todo variabel scope (lingkup variabel)
+// * block scope dan functoin scope (javascript)
+
+// todo Rekursif
+// * Base case = kondisi akhir dari rekursif yang menghasilkan nilai.
+// * Semua looping bisa dibuat rekursif, tapi tidak sebaliknya.
+// * Implementasi Rekursif
+// * 1. Menggantikan looping
+// * 2. Fibonacci
+// * 3. Pencarian dan penelusuran pada struktur data list dan tre
+// * 4. Bahasa pemrograman yang tidak memiliki pengulangan seperti Haskel, Erlang, Prolog, dll
+// * function cetakAngka(n) {
+//     // * Base Case
+//     if (n === 0) {
+//         return;
+//     }
+//     // * End Base Case
+//     console.log(n);
+//     cetakAngka(n - 1);
+// }
+// cetakAngka(10);
+// console.log(cetakAngka(10));
+// ? Contoh rekursif pada faktorial
+// function faktroial(n) {
+//     if (n === 0) {
+//         return 1;
+//     }
+//     return n * faktroial(n - 1);
+// }
+// console.log(faktroial(5));
+// ! End Rekursif
+
+// todo Declaration dan Expression
+// ? Contoh function declaration -> bisa panggil dulu baru deklarasi atau sebaliknya
+// function tampilPesan(nama) {
+//     console.log('Halo ' + nama + '.');
+// }
+// tampilPesan('Function Declarations');
+// ? Contoh function expression -> harus deklarasi dulu baru dipanggil
+// var tampilPesan = function (nama) {
+//     console.log('Halo ' + nama + '.');
+// }
+// tampilPesan('Function Expression');
+// ! End Declaration dan Expression
+
+// todo Array
+// * Mempermudah pengelolaan nilai/value/data -> penelusuran dan pencarian
+// * Manajemen memori
+var myFunc = function () {
+    console.log('Hello World!');
 }
-var coba = tambahPseudo(1, 2, 3, 4, 5);
-console.log(coba);
+// ? Contoh penulisan array
+var myArr3 = [];
+myArr3 = ['teks', 2, false, myFunc, [4, 5, 6]];
+// console.log(myArr3[4][1]); // output 5
+console.log(myArr3[3]);
+// ! End Array
